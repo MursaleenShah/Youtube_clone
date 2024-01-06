3bd70bbc6424ab3b4afb86ebf5414e296d30a1d8
MursaleenShah <shahmursalin9@gmail.com>
2024-01-07 00:10:05 +0530

    Add website layout

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
-import React from 'react'
+import {Navbar, VideoDetail,ChannelDetail, SearchFeed,Feed} from './components/index.js';
+import {BrowserRouter , Route,Routes} from 'react-router-dom';
+import {Box} from '@mui/material'
 
 export default function App() {
   return (
-    <div>App</div>
+    <BrowserRouter>
+  <Box sx={{ backgroundColor : '#000'}}>
+    <Navbar />
+    <Routes>
+      <Route path='/' exact element={<Feed />} />
+      <Route path='/video/:id'  element={<VideoDetail />} />
+      <Route path='/channel/:id'  element={<ChannelDetail />} />
+      <Route path='/search/:searchTerm'  element={<SearchFeed />} />
+    </Routes>
+  </Box>
+  </BrowserRouter>
   )
+  
 }
