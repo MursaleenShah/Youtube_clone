7b9eaad1ba9abc4c3d9b810019f567f54903d211
MursaleenShah <shahmursalin9@gmail.com>
2024-01-11 14:06:37 +0530

    partial VideoDetail component done

--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -1,7 +1,36 @@
-import React from 'react'
+import {useState, useEffect} from 'react';
+import { Link, useParams } from 'react-router-dom';
+import ReactPlayer from 'react-player';
+import { Typography,Box,Stack } from '@mui/material';
+import { CheckCircle } from '@mui/icons-material';
+import {Video} from './';
+import { fetchFromAPI } from '../utils/fetchFromAPI';
 
 export default function VideoDetail() {
+  const [VideoDetail, setVideoDetail] = useState(null);
+  const {id} = useParams();
+  const {snippet:{title,channedId, channelTitle},
+  statistics:{viewCount,likeCount}} = VideoDetail;
+  useEffect(()=> {
+    fetchFromAPI(`videos?part=snippet,statistics&id=${id}`)
+    .then((data)=> setVideoDetail(data.items[0]))
+  },[id])
+
   return (
-    <div>VideoDetail</div>
+    <Box minHeight='95vh'>
+      <Stack direction={{xs:'column',md:'row'}}>
+        <Box flex={1}>
+          <Box sx={{width:'100%',position:'sticky',top:'86px'}}>
+            <ReactPlayer url={`https://www.youtube.com/watch?v=${id}`}
+            className="react-player" controls />
+             <Typography color="#fff" variant="h5" fontWeight="bold" p={2}>
+              {title}
+            </Typography>
+          </Box>
+
+        </Box>
+      </Stack>
+
+    </Box>
   )
 }
